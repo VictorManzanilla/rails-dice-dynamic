2c3cd68dcf427e3f42d827350033743a0d71f532
Victor Manzanilla <67130786+VictorManzanilla@users.noreply.github.com>
2025-04-29 21:19:39 +0000

    done

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -27,4 +27,39 @@ class DiceController < ApplicationController
     render({ :template => "dice_templates/two_ten"})
   end
 
+  def one_twenty
+    @rolls = []
+
+  1.times do
+    die = rand(1..20)
+
+    @rolls.push(die)
+  end
+    render({ :template => "dice_templates/one_twenty"})
+  end
+
+  def five_four
+    @rolls = []
+
+    5.times do
+      die = rand(1..4)
+  
+      @rolls.push(die)
+    end
+      render({ :template => "dice_templates/five_four"})
+  end
+
+  def dynamic
+    @num_dice = params.fetch("number_of_dice").to_i
+    @num_sides = params.fetch("number_of_sides").to_i
+
+    @rolls = []
+
+    @num_dice.times do
+      die = rand(1..@num_sides)
+  
+      @rolls.push(die)
+    end
+      render({ :template => "dice_templates/dynamic"})
+  end
 end
